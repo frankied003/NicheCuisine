0f69236ce86c6fb83c975063dd461071215cfc05
ahernan5 <ahernan5@stevens.edu>
2022-03-23 23:56:51 -0400

    welcomeScreen edit

--- niche-cuisine-expo/Screens/signedOutScreens/welcomeScreen.js
+++ niche-cuisine-expo/Screens/signedOutScreens/welcomeScreen.js
@@ -1,4 +1,5 @@
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, View, Image,} from 'react-native';
+
 
 // Stacks
 
@@ -9,9 +10,9 @@ export default function WelcomeScreen({ navigation }) {
             style= {{width:400, height:300}}
                 source={require('niche-cuisine-expo/assets/Niche.png')}
             />
-            <Text style={styles.setFontSizeOne}>Sign up to continue</Text>
-            <Button title='Login' onPress={() => navigation.navigate("Login")} />
-            <Button title='Signup' onPress={() => navigation.navigate("Signup")} />
+            <Text style={styles.setFontSizeOne}>Sign up or Login to continue!</Text>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}><Text style={styles.setFontSizeTwo}>Login</Text></TouchableOpacity> 
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Signup")}><Text style={styles.setFontSizeTwo}>Signup</Text></TouchableOpacity> 
         </View>
     );
 }
@@ -22,8 +23,31 @@ const styles = StyleSheet.create({
         backgroundColor: '#F6F4F1',
         alignItems: 'center',
         justifyContent: 'center',
+        marginBottom: 80
     },
     setFontSizeOne:{
-        fontSize: 26
+        marginTop: 20,
+        marginBottom: 10,
+        fontSize: 20
     },
+    setFontSizeTwo:{
+        fontSize: 20,
+        color: 'white'
+    
+    },
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: "#A68258",
+        width: 300,
+        height: 50,
+        borderColor: "#FFFFFF",
+        borderWidth: 2,
+        borderRadius: 5,
+        marginTop: 15,
+        padding: 10     
+     },
 });
