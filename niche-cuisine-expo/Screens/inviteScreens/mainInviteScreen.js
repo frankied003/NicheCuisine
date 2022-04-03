5168fe5c3a3dea9f1ce4f44315c6d53d736dbac6
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2022-04-03 12:25:27 -0400

    Init

---
+++ niche-cuisine-expo/Screens/inviteScreens/mainInviteScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToggleSwitch from './ToggleSwitch/ToggleSwitch'
+
+function App() {
+    return (
+      <ToggleSwitch />
+    );
+  }
+  
+function App() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Try editing me! 🎉
+        </Text>
+      </View>
+    );
+  }
+  
+ 
+  
+  export default App;
