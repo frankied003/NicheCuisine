b34fb0c8e9f32deb5b78af7c3a33549a6a565a54
jdmorrone01 <74426727+jdmorrone01@users.noreply.github.com>
2022-04-20 21:16:01 -0400

    some styling and added information

--- niche-cuisine-expo/Screens/signedInScreens/viewMealScreen.js
+++ niche-cuisine-expo/Screens/signedInScreens/viewMealScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView, Button ,SectionList} from 'react-native';
 
 
+
 export default function ViewMealScreen(props) {
 
     const { navigation, route } = props;
@@ -9,18 +10,21 @@ export default function ViewMealScreen(props) {
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Text>View Meal Screen</Text>
+            <Text> Host: {route.params.data.userName}</Text>
             <Text>{route.params.data.mealName}</Text>
-            <Text>Location:{route.params.data.location}</Text>
-            <Text>Time:{route.params.data.time}</Text>
-            <Text>Price:{route.params.data.price}</Text>
+            <Text>Location: {route.params.data.location}</Text>
+            <Text>Time: {route.params.data.time}</Text>
+            <Text>Price: {route.params.data.price}</Text>
             
 
             {
             route.params.data.subMeals.map((subMeal) => {
                 return ( 
-                    <View>
-                    <Text>{subMeal.type}</Text>
-                    <Text>{subMeal.name}</Text>
+                    <View style = {styles.smc}>
+                    <Text>Course: {subMeal.type}</Text>
+                    <Text>Title: {subMeal.name}</Text>
+                    <Text>Description: {subMeal.description}</Text>
+                    <Text>Allergiens:</Text>
                    </View>                
         );
             })}
@@ -36,5 +40,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F6F4F1',
+    },
+    smc:{
+        borderWidth: 2,
+        borderColor: 'blue',
+
     }
 });
