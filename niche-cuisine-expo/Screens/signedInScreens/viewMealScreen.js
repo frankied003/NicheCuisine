12c9ebc8accc99ebbecc5aaeb5324bfa8e0c6519
jdmorrone01 <74426727+jdmorrone01@users.noreply.github.com>
2022-04-22 14:53:03 -0400

    added styling & information to detailed view meal

--- niche-cuisine-expo/Screens/signedInScreens/viewMealScreen.js
+++ niche-cuisine-expo/Screens/signedInScreens/viewMealScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView, Button ,SectionList} from 'react-native';
 
-
+// import profilePic from '../assets/profilepic.png';
+// import noImage from '../assets/no-image.jpg';
+// import Tag from '../tag';
 
 export default function ViewMealScreen(props) {
 
@@ -9,22 +12,29 @@ export default function ViewMealScreen(props) {
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text>View Meal Screen</Text>
-            <Text> Host: {route.params.data.userName}</Text>
-            <Text>{route.params.data.mealName}</Text>
-            <Text>Location: {route.params.data.location}</Text>
-            <Text>Time: {route.params.data.time}</Text>
-            <Text>Price: {route.params.data.price}</Text>
+             <Image source={require('niche-cuisine-expo/assets/no-image.jpg')} style={styles.mainImage} />
+            <Text style = {styles.mealTitle}>{route.params.data.mealName}</Text>
+            <View style = {styles.topHeaderContainer}> 
+            <Image source={require('niche-cuisine-expo/assets/profilepic.png')} style={styles.profileImage} />
+            <View>
+            <Text style ={styles.profileNameText}>Host: {route.params.data.userName}</Text>
+            <Text style ={styles.profileNameText}>Location: {route.params.data.location}</Text>
+            <Text style ={styles.profileNameText}>Time: {moment.unix(route.params.data.time).format('MMMM Do, h:mm A')}</Text>
+            <Text style ={styles.profileNameText}>Price: ${route.params.data.price}</Text>
+            </View>
+            </View>
+            
+            
             
 
             {
             route.params.data.subMeals.map((subMeal) => {
                 return ( 
                     <View style = {styles.smc}>
-                    <Text>Course: {subMeal.type}</Text>
-                    <Text>Title: {subMeal.name}</Text>
+                    <Text style = {styles.title}>{subMeal.type}:<Text style = {styles.subT}>{subMeal.name}</Text></Text>
                     <Text>Description: {subMeal.description}</Text>
-                    <Text>Allergiens:</Text>
+                    <Text>Ingredients: {subMeal.ingredients.map((ingredent)=> {return(<Text>{ingredent} </Text>);})}</Text>
+                    <Text>Allergiens: {subMeal.allergens.map((allergen)=> {return(<Text>{allergen} </Text>);})}</Text>
                    </View>                
         );
             })}
@@ -43,7 +53,45 @@ const styles = StyleSheet.create({
     },
     smc:{
         borderWidth: 2,
-        borderColor: 'blue',
+        borderColor: '#A68258',
 
-    }
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize: 20,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    mainImage: {
+        width: '100%',
+        height: 250
+    },
+    profileNameText: {
+        marginLeft: 5,
+        fontSize: 13,
+        fontWeight: '600'
+    },
+    subT:{
+        fontSize: 15,
+        fontWeight: 'normal',
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 20,
+    },
+    mealTitle:{
+        fontWeight:'bold',
+        fontSize: 20,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    topHeaderContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    
 });
