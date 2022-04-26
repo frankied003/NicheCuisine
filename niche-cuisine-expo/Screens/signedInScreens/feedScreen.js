58d207e6566db1791c0371e926ef78dfef298a21
Frank DiGiacomo <frankied003@hotmail.com>
2022-04-26 14:15:56 -0400

    Added search meal

--- niche-cuisine-expo/Screens/signedInScreens/feedScreen.js
+++ niche-cuisine-expo/Screens/signedInScreens/feedScreen.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, FlatList, RefreshControl, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, FlatList, RefreshControl, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+
 import { getRequest } from '../../Api/api';
 import MealCard from '../../Components/mealCard';
-
-// Components
 
 export default function FeedScreen({ navigation }) {
 
@@ -11,6 +11,8 @@ export default function FeedScreen({ navigation }) {
     const [meals, setmeals] = useState([]);
     const [refreshing, setrefreshing] = useState(false);
     const [error, seterror] = useState(null);
+
+    const [searchInput, setsearchInput] = useState('');
 
     useEffect(async () => {
         let newMeals = await getRequest('/getMeals');
@@ -26,6 +28,19 @@ export default function FeedScreen({ navigation }) {
         if (newMeals.length > 0) {
             setmeals(newMeals);
         }
+        return newMeals;
+    }
+
+    const search = async () => {
+        setrefreshing(true)
+        console.log(searchInput)
+        let newMeals = await loadMore();
+        newMeals = newMeals.filter(function (meal) {
+            return meal.mealName.startsWith(searchInput);
+        });
+        setmeals(newMeals)
+        setrefreshing(false)
+
     }
 
     const renderItem = ({ item }) => (
@@ -37,16 +52,28 @@ export default function FeedScreen({ navigation }) {
             {loadingMeals
                 ? <ActivityIndicator size='large' style={styles.activityIndicator} />
                 : (
-                    <FlatList
-                        refreshControl={
-                            <RefreshControl refreshing={refreshing} onRefresh={() => loadMore()} />
-                        }
-                        data={meals}
-                        renderItem={renderItem}
-                        contentContainerStyle={styles.mainContainer}
-                        keyExtractor={meal => meal.id}
-                        key={meal => meal.id}
-                    />
+                    <View style={styles.mainContainer}>
+                        <View style={styles.searchBoxContainer}>
+                            <TextInput
+                                value={searchInput}
+                                style={styles.searchBox}
+                                onChangeText={(text) => setsearchInput(text)}
+                                placeholder="Search for meal"
+                            />
+                            <TouchableOpacity onPress={() => search()}>
+                                <Icon name='search' type='material' color='#000000' />
+                            </TouchableOpacity>
+                        </View>
+                        <FlatList
+                            refreshControl={
+                                <RefreshControl refreshing={refreshing} onRefresh={() => loadMore()} />
+                            }
+                            data={meals}
+                            renderItem={renderItem}
+                            keyExtractor={meal => meal.id}
+                            key={meal => meal.id}
+                        />
+                    </View>
                 )
             }
         </>
@@ -58,8 +85,25 @@ const styles = StyleSheet.create({
         marginTop: 6,
         marginLeft: 6,
         marginRight: 6,
+        paddingBottom: 40
     },
     activityIndicator: {
         margin: 20
+    },
+    searchBoxContainer: {
+        width: '100%',
+        height: 35,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingRight: 10
+    },
+    searchBox: {
+        width: '90%',
+        height: '100%',
+        paddingLeft: 10
     }
 });
